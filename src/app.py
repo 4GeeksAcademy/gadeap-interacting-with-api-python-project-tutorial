a853ed06767582c63cf0e8d8783110b1528acf42
GadeaP <148185671+GadeaP@users.noreply.github.com>
2024-02-07 18:23:32 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -1,15 +1,32 @@
 import os
-from sqlalchemy import create_engine
-import pandas as pd
 from dotenv import load_dotenv
-
-# load the .env file variables
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+con = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id,
+                                                              client_secret = client_secret))
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+artist_id = "0WwSkZ7LtFUFjGjMZBMt6T"
+
+response = sp.artist_top_tracks("0WwSkZ7LtFUFjGjMZBMt6T")
+if response:
+  tracks = response["tracks"]
+  tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]
+
+
+
+import pandas as pd
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace = True)
+print(tracks_df.head(3))
+
+
+import seaborn as sns
+scatter_plot = sns.scatterplot(data = tracks_df, x = "popularity", y = "duration_ms")
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
